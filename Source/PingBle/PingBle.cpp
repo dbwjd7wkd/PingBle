492d3abe9207df84645401e540589fa06ae1b3b3
YuJeong <dbwjd7wkd@gmail.com>
2021-06-26 16:22:35 +0900

    renew

---
+++ Source/PingBle/PingBle.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "PingBle.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, PingBle, "PingBle" );
