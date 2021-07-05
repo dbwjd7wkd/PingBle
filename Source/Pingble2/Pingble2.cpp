fc0e47d10480f8c64d54eb78ad6755d294ecc364
YuJeong <dbwjd7wkd@gmail.com>
2021-07-05 10:14:17 +0900

    유정 프로젝트 삭제

--- Source/Pingble2/Pingble2.cpp
+++
@@ -1,7 +0,0 @@
-// Copyright Epic Games, Inc. All Rights Reserved.
-
-#include "Pingble2.h"
-#include "Modules/ModuleManager.h"
-
-IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, Pingble2, "Pingble2" );
- 
